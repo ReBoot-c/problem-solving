ad025d0fdf8bd38fc149879e926c99603f62206f
ReBoot-c <stegnov00@bk.ru>
2023-02-15 20:07:56 +0300

    Update 66.cpp

--- acmp/66.cpp
+++ acmp/66.cpp
@@ -3,28 +3,28 @@
 
 using namespace std;
 /* Задача: https://acmp.ru/index.asp?main=task&id_task=66
-Для данной буквы английского алфавита нужно вывести справа стоящую букву на стандартной клавиатуре. 
-При этом клавиатура замкнута, т.е. справа от буквы «p» стоит буква «a», от буквы «l» стоит буква «z», 
+Для данной буквы английского алфавита нужно вывести справа стоящую букву на стандартной клавиатуре.
+При этом клавиатура замкнута, т.е. справа от буквы «p» стоит буква «a», от буквы «l» стоит буква «z»,
 а от буквы «m» — буква «q».
 */
 int main() {
-    const string KEYBOARD = "qwertyuiopasdfghjklzxcvbnmq";
+    const string KEYBOARD = "qwertyuiopasdfghjklzxcvbnm";
 
     char character;
     cin >> character;
 
     // 1 способ
-    for (int i = 0; i < KEYBOARD.length();i++) { 
+    for (int i = 0; i < KEYBOARD.length(); i++) {
         if (KEYBOARD[i] == character) {
-            cout << KEYBOARD[i % KEYBOARD.length()] << endl;
+            cout << KEYBOARD[(i + 1) % KEYBOARD.length()] << endl;
             break;
         }
     }
 
     // 2 способ
-    int index = KEYBOARD.find(character); 
+    int index = KEYBOARD.find(character);
     if (index != string::npos) {
-        cout << KEYBOARD[index % KEYBOARD.length()] << endl;
+        cout << KEYBOARD[(index + 1) % KEYBOARD.length()] << endl;
     }
 
     return 0;
