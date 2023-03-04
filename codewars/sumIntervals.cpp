49ea47edd6d29aec98a1da23153360d464f423d5
ReBoot-c <stegnov00@bk.ru>
2023-03-04 15:19:05 +0300

    Update sumIntervals.cpp

--- codewars/sumIntervals.cpp
+++ codewars/sumIntervals.cpp
@@ -4,15 +4,32 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
-#include <iostream>
 
 int sum_intervals(std::vector<std::pair<int, int>> intervals) {
-  std::sort(intervals.begin(), intervals.end());
-  for (const auto i : intervals) std::cout << i.first << " " << i.second << ", ";
-  return 0;
+	std::sort(intervals.begin(), intervals.end());
+	int first = intervals[0].first;
+	int second = intervals[0].second;
+	int summary = 0;
+
+	if (intervals.size() == 1) {
+		return second - first;
+	}
+
+	for (int i = 1; i < intervals.size(); i++) {
+		int firstCurrent = intervals[i].first;
+		int secondCurrent = intervals[i].second;
+
+		if (firstCurrent > second) {
+			summary += second - first;
+			first = firstCurrent;
+			second = secondCurrent;
+		}
+		else {
+			second = std::max(second, secondCurrent);
+		}
+	}
+	summary += second - first;
+
+	return summary;
 }
-// FIXME: НЕ РАБОТАЕТ!!!
-int main() {
-  sum_intervals(std::vector<std::pair<int,int>>({{100,1000}, {9, 20}}));
-  return 0;
-}
+
